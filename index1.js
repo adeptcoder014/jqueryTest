343861a20ea4ac618e0a7323ff1f9bbf406b311f
Nischal <gupta.nischal014@gmail.com>
2023-03-02 11:03:50 +0530

    completed the task with jQuery

--- index1.js
+++ index1.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  //================ TOGGLE_INSTA_FIELD_OPTION ===================================================
+
   $("#toggleInput").change(function () {
     if ($(this).is(":checked")) {
       $("#inputField").show();
@@ -6,59 +8,140 @@ $(document).ready(function () {
       $("#inputField").hide();
     }
   });
-
+  //================ GET_PROPER_EMPLOYEE_ID(based on the records size) ===================================================
   $("#getId").click(function () {
-    var randomId = Math.floor(Math.random() * 1000000);
-    $("#postId").val(randomId);
+    let emp = localStorage.getItem("formDataArray");
+
+    let length = JSON.parse(emp).length;
+    console.log(length);
+    var id = length + 1;
+    $("#postId").val(id);
   });
+  //================ MOBILE_NUMBER_VALIDATION ===================================================
 
   $("#mobile").blur(function () {
     var mobileValue = $(this).val();
-    if (mobileValue.length < 10) {
+    if (mobileValue.length < 10 || mobileValue.length > 10) {
       $("#mobileError").show();
     } else {
       $("#mobileError").hide();
     }
   });
+  //================ MAPPING AND DISPLAYING THE ENTERED DATA ===================================================
 
-  // const localStorageData = localStorage.getItem("formDataArray");
-  // const data = JSON.parse(localStorageData);
+  let emp = localStorage.getItem("formDataArray");
 
-  // $.each(data, function (index, data) {
-  //   var row = $("<tr>").attr("id", data.id);
+  $.each(JSON.parse(emp), function (index, data) {
+    var row = $("<tr>").attr("id", data.id);
+    var editBtn = $("<button>").text("Edit");
+    var deleteBtn = $("<button>").text("Delete");
+    row.append($("<td>").text("TYPE1"));
 
-  //   row.append($("<td>").text(data.id));
-  //   row.append($("<td>").text(data.name));
-  //   row.append($("<td>").text(data.mobile));
-  //   row.append($("<td>").text(data.gender));
-  //   row.append($("<td>").text(data.dob));
-  //   row.append($("<td>").text(data.instaId));
-  //   row.append($("<td>").append("editBtn"));
+    row.append($("<td>").text(data.id));
 
-  //   row.append($("<td>").text("Delete"));
+    row.append($("<td>").text(data.name));
+    row.append($("<td>").text(data.mobile));
+    row.append($("<td>").text(data.gender));
+    row.append($("<td>").text(data.dob));
+    row.append($("<td>").text(data.instaId));
 
-  //   $("#myTable tbody").append(row);
+    row.append($("<td>").append(editBtn).addClass("editBtn"));
+    row.append($("<td>").append(deleteBtn).addClass("deleteBtn"));
 
-  // });
+    $("#myTable tbody").append(row);
 
-  // const mappedData = data.map((x) => x);
-  // console.log(mappedData);
+    editBtn.click(function () {
+      if (editBtn.text() === "Edit") {
+        editBtn.text("Save");
 
+        $("#name").val(data.name);
+
+        $("#mobile").val(data.mobile);
+        $("#gender").val(data.gender);
+        $("#dob").val(data.dob);
+        $("#insta").val(data.instaId);
+        $("#name").on("input", function () {
+          data.name = $(this).val();
+        });
+        $("#mobile").on("input", function () {
+          data.mobile = $(this).val();
+        });
+        $("#gender").on("input", function () {
+          data.gender = $("input[name='gender']:checked").val();
+        });
+        $("#dob").on("input", function () {
+          data.dob = $(this).val();
+        });
+        $("#insta").on("input", function () {
+          data.insta = $(this).val();
+        });
+      } else {
+        editBtn.text("Edit");
+        // console.log(emp)
+        data.name = $("#name").val();
+
+        emp = JSON.stringify(
+          JSON.parse(emp).map(function (item) {
+            return item.id == data.id ? data : item;
+          })
+        );
+
+        // Update the data in localStorage
+        localStorage.setItem("formDataArray", emp);
+      }
+    });
+    deleteBtn.click(function () {
+      $(this).closest("tr").remove(); // remove the row from the table
+      // update the data in localStorage
+      var formDataArray = JSON.parse(localStorage.getItem("formDataArray"));
+      var newData = formDataArray.filter(function (obj) {
+        return obj.id !== data.id;
+      });
+      localStorage.setItem("formDataArray", JSON.stringify(newData));
+    });
+
+    //============= VALIDATION ===================
+    var name = $("#name").val();
+    var email = $("#email").val();
+
+    // Validate form
+    // if (!name || !email) {
+    //   alert("Please fill in all fields");
+    //   return;
+    // }
+
+    // =======================================
+  });
+
+  //======== TABLE_SEARCH_FUNCTIONALITY (using name as a search parameter)====================================
+  $("#searchInput").on("input", function () {
+    var searchValue = $(this).val().toLowerCase();
+
+    $("#myTable tbody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(searchValue) > -1);
+    });
+  });
+
+  //================ FORM_SUBMISSION ===================================================
   $("#myForm").submit(function (event) {
     event.preventDefault();
 
-    var formData = $(this).serializeArray(); // Serialize form data as an array
+    var formData = $(this).serializeArray();
 
     var formData = {
       name: $("#name").val(),
       mobile: $("#mobile").val(),
       employeeId: $("#postId").val(),
       employeeType: $("#empType").val(),
-      gender: $("#gender").val(),
+      gender: $("input[name='gender']:checked").val(),
       dob: $("#dob").val(),
       instaId: $("#insta").val(),
       id: Math.floor(Math.random() * 1000000),
     };
+    // Validate form
+    if (!formData.name || !formData.dob || !formData.gender) {
+      return alert("Please fill in all fields");
+    }
 
     let formDataArray = [];
     formDataArray.push(formData);
@@ -68,87 +151,23 @@ $(document).ready(function () {
     if (existingData) {
       formDataArray = JSON.parse(existingData);
       formDataArray.push(formData);
+      //--------------UNIQUE_ENRIES_USING_MOBILE ---------------------
+      var mobile = $("#mobile").val();
+      if (existingData.includes(mobile)) {
+        return alert("Already entered for this person");
+      } else {
+        window.location.reload();
+      }
     }
 
     // Store the data in local storage
     localStorage.setItem("formDataArray", JSON.stringify(formDataArray));
 
-    console.log(formDataArray);
-    $.each(formDataArray, function (index, data) {
-      var row = $("<tr>").attr("id", data.id);
-      var editBtn = $("<button>").text("Edit");
-
-      row.append($("<td>").text(data.id));
-      row.append($("<td>").text(data.name));
-      row.append($("<td>").text(data.mobile));
-      row.append($("<td>").text(data.gender));
-      row.append($("<td>").text(data.dob));
-      row.append($("<td>").text(data.instaId));
-      // Edit button
-      editBtn.click(function () {
-        // Fill the form fields with the data of this row
-        $("#name").val(data.name);
-        $("#mobile").val(data.mobile);
-        $("#gender").val(data.gender);
-        $("#dob").val(data.dob);
-        $("#insta").val(data.instaId);
-
-        // Change the button text to "Save"
-        editBtn.text("Save");
-        let inputName = "";
-        $("#name").change(function () {
-          inputName = $(this).val();
-
-          // Update the data object with the new value
-          // data.name = inputName;
-          // // Update the table cell with the new value
-          // $("#" + data.id)
-          //   .children("td:nth-child(2)")
-          //   .text(inputName);
-          // // Store the updated data in local storage
-          // localStorage.setItem("formDataArray", JSON.stringify(formDataArray));
-        });
-        // Update the row with the new data when the "Save" button is clicked
-       console.log("|||---------",inputName)
-        editBtn.click(function () {
-          data.name = inputName;
-          $("#" + data.id)
-            .children("td:nth-child(2)")
-            .text(inputName);
-          // $("#" + data.id)
-          //   .children("td:nth-child(3)")
-          //   .text($("#mobile").val());
-          // $("#" + data.id)
-          //   .children("td:nth-child(4)")
-          //   .text($("#gender").val());
-          // $("#" + data.id)
-          //   .children("td:nth-child(5)")
-          //   .text($("#dob").val());
-          // $("#" + data.id)
-          //   .children("td:nth-child(6)")
-          //   .text($("#insta").val());
-
-          // Change the button text back to "Edit"
-
-          // Update the data object with the new values
-
-          editBtn.text("Edit");
-          // Store the updated data in local storage
-          localStorage.setItem("formDataArray", JSON.stringify(formDataArray));
-        });
-      });
-      row.append($("<td>").append(editBtn));
-      row.append($("<td>").text("Delete"));
-
-      $("#myTable tbody").append(row);
-    });
-
+    // clear the form
     $("#name").val("");
     $("#mobile").val("");
     $("#gender").val("male");
     $("#dob").val("");
     $("#insta").val("");
-
-    // console.log("-------------------------", formData);
   });
 });
